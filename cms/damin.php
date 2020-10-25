a59665cfb24aaeba1f276f5890ee32eeb661dacf
aepsaefulloh <aepsaefulloh1396@gmail.com>
2020-10-25 18:30:19 +0700

    login view

--- cms/damin.php
+++ cms/damin.php
@@ -14,20 +14,20 @@ if($_SESSION['ISLOGIN']){
 
 ?>
 <!DOCTYPE html>
-
-</html>
 <html lang="en">
+<!--begin::Head-->
 
 <head>
     <meta charset="utf-8" />
     <title>Login</title>
-    <meta name="description" content="" />
+    <meta name="description" content="Singin page example" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <link rel="canonical" href="https://keenthemes.com/metronic" />
     <!--begin::Fonts-->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
     <!--end::Fonts-->
     <!--begin::Page Custom Styles(used by this page)-->
-    <link href="<?php echo CMS_URL?>/assets/css/pages/login/classic/login-4.css" rel="stylesheet" type="text/css" />
+    <link href="<?php echo CMS_URL?>/assets/css/pages/login/login-3.css" rel="stylesheet" type="text/css" />
     <!--end::Page Custom Styles-->
     <!--begin::Global Theme Styles(used by all pages)-->
     <link href="<?php echo CMS_URL?>/assets/plugins/global/plugins.bundle.css" rel="stylesheet" type="text/css" />
@@ -41,7 +41,7 @@ if($_SESSION['ISLOGIN']){
     <link href="<?php echo CMS_URL?>/assets/css/themes/layout/brand/dark.css" rel="stylesheet" type="text/css" />
     <link href="<?php echo CMS_URL?>/assets/css/themes/layout/aside/dark.css" rel="stylesheet" type="text/css" />
     <!--end::Layout Themes-->
-    <link rel="shortcut icon" href="<?php echo CMS_URL?>/assets/media/logos/favicon.ico" />
+    <link rel="shortcut icon" href="<?php echo ROOT_URL?>/assets/img/icon/favicon/favicon.ico?<?php echo rand()?>" />
 </head>
 <!--end::Head-->
 <!--begin::Body-->
@@ -51,114 +51,84 @@ if($_SESSION['ISLOGIN']){
     <!--begin::Main-->
     <div class="d-flex flex-column flex-root">
         <!--begin::Login-->
-        <div class="login login-4 login-signin-on d-flex flex-row-fluid" id="kt_login">
-            <div class="d-flex flex-center flex-row-fluid bgi-size-cover bgi-position-top bgi-no-repeat"
-                style="background-image: url('<?php echo CMS_URL?>/assets/media/bg/bg-3.jpg');">
-                <div class="login-form text-center p-7 position-relative overflow-hidden">
-                    <!--begin::Login Header-->
-                    <div class="d-flex flex-center mb-5">
-                        <a href="#">
-                            <img src="<?php echo CMS_URL?>/assets/img/logo.png?<?php echo rand()?>" class="max-h-100px"
-                                alt="" />
-                        </a>
-                    </div>
-                    <!--end::Login Header-->
-                    <!--begin::Login Sign in form-->
-                    <div class="login-signin">
-                        <div class="mb-20">
-                            <h3>Sign In To Admin</h3>
-                            <div class="text-muted font-weight-bold">Enter your details to login to your account:</div>
-                        </div>
-                        <form class="form" action="damin.php" method="post">
-                            <form id="login">
-                                <input type='hidden' name='act' value='login'>
-                                <div class="form-group mb-5">
-                                    <input class="form-control h-auto form-control-solid py-4 px-8" type="text"
-                                        placeholder="Username" name="username" autocomplete="off" />
-                                </div>
-                                <div class="form-group mb-5">
-                                    <input class="form-control h-auto form-control-solid py-4 px-8" type="password"
-                                        placeholder="Password" name="password" />
-                                </div>
-                                <button id="kt_login_signin_submit"
-                                    class="btn btn-primary font-weight-bold px-9 py-4 my-3 mx-4">Sign In</button>
-                            </form>
-
-                    </div>
-
+        <div class="login login-3 wizard d-flex flex-column flex-lg-row flex-column-fluid">
+            <!--begin::Aside-->
+            <div class="login-aside d-flex flex-column flex-row-auto">
+                <!--begin::Aside Top-->
+                <div class="d-flex flex-column-auto flex-column pt-lg-40 pt-15">
+                    <!--begin::Aside header-->
+                    <a href="#" class="login-logo text-center pt-lg-25 pb-10">
+                        <img src="<?php echo CMS_URL?>/assets/img/logo.png?<?php echo rand()?>" class="" alt="" />
+                    </a>
+                    <!--end::Aside header-->
+                    <!--begin::Aside Title-->
+                    <h3 class="font-weight-bolder text-center font-size-h4 text-dark-50 line-height-xl">User Experience
+                        &amp; Interface Design
+                        <br />Strategy Solutions
+                    </h3>
+                    <!--end::Aside Title-->
                 </div>
+                <!--end::Aside Top-->
+                <!--begin::Aside Bottom-->
+                <div class="aside-img d-flex flex-row-fluid bgi-no-repeat bgi-position-x-center"
+                    style="background-position-y: calc(100% + 5rem); background-image: url(<?php echo CMS_URL?>/assets/media/svg/illustrations/login-visual-5.svg)">
+                </div>
+                <!--end::Aside Bottom-->
             </div>
+            <!--begin::Aside-->
+            <!--begin::Content-->
+            <div class="login-content flex-row-fluid d-flex flex-column p-10">
+                <!--begin::Wrapper-->
+                <div class="d-flex flex-row-fluid flex-center">
+                    <!--begin::Signin-->
+                    <div class="login-form">
+                        <!--begin::Form-->
+                        <form class="form" action="damin.php" method="post">
+                            <input type='hidden' name='act' value='login'>
+                            <div class="pb-5">
+                                <h3 class="font-weight-bolder text-dark font-size-h2 font-size-h1-lg">Sign In</h3>
+                            </div>
+                            <div class="form-group">
+                                <label class="font-size-h6 font-weight-bolder text-dark">Username</label>
+                                <input class="form-control h-auto py-7 px-6 rounded-lg border-0" type="text"
+                                    name="username" autocomplete="off" />
+                            </div>
+                            <!--end::Form group-->
+                            <!--begin::Form group-->
+                            <div class="form-group">
+                                <div class="d-flex justify-content-between mt-n5">
+                                    <label class="font-size-h6 font-weight-bolder text-dark pt-5">Password</label>
+                                </div>
+                                <input class="form-control h-auto py-7 px-6 rounded-lg border-0" type="password"
+                                    name="password" autocomplete="off" />
+                            </div>
+                            <!--end::Form group-->
+                            <!--begin::Action-->
+                            <div class="pb-lg-0 pb-5">
+                                <button type="submit" id="kt_login_singin_form_submit_button"
+                                    class="btn btn-primary font-weight-bolder font-size-h6 px-8 py-4 my-3 mr-3">Sign
+                                    In</button>
+                            </div>
+                            <!--end::Action-->
+                        </form>
+                        <!--end::Form-->
+                    </div>
+                    <!--end::Signin-->
+                </div>
+                <!--end::Wrapper-->
+            </div>
+            <!--end::Content-->
         </div>
         <!--end::Login-->
     </div>
-    <script>
-    var KTAppSettings = {
-        "breakpoints": {
-            "sm": 576,
-            "md": 768,
-            "lg": 992,
-            "xl": 1200,
-            "xxl": 1400
-        },
-        "colors": {
-            "theme": {
-                "base": {
-                    "white": "#ffffff",
-                    "primary": "#3699FF",
-                    "secondary": "#E5EAEE",
-                    "success": "#1BC5BD",
-                    "info": "#8950FC",
-                    "warning": "#FFA800",
-                    "danger": "#F64E60",
-                    "light": "#E4E6EF",
-                    "dark": "#181C32"
-                },
-                "light": {
-                    "white": "#ffffff",
-                    "primary": "#E1F0FF",
-                    "secondary": "#EBEDF3",
-                    "success": "#C9F7F5",
-                    "info": "#EEE5FF",
-                    "warning": "#FFF4DE",
-                    "danger": "#FFE2E5",
-                    "light": "#F3F6F9",
-                    "dark": "#D6D6E0"
-                },
-                "inverse": {
-                    "white": "#ffffff",
-                    "primary": "#ffffff",
-                    "secondary": "#3F4254",
-                    "success": "#ffffff",
-                    "info": "#ffffff",
-                    "warning": "#ffffff",
-                    "danger": "#ffffff",
-                    "light": "#464E5F",
-                    "dark": "#ffffff"
-                }
-            },
-            "gray": {
-                "gray-100": "#F3F6F9",
-                "gray-200": "#EBEDF3",
-                "gray-300": "#E4E6EF",
-                "gray-400": "#D1D3E0",
-                "gray-500": "#B5B5C3",
-                "gray-600": "#7E8299",
-                "gray-700": "#5E6278",
-                "gray-800": "#3F4254",
-                "gray-900": "#181C32"
-            }
-        },
-        "font-family": "Poppins"
-    };
-    </script>
-    <!--end::Global Config-->
+
     <!--begin::Global Theme Bundle(used by all pages)-->
     <script src="<?php echo CMS_URL?>/assets/plugins/global/plugins.bundle.js"></script>
     <script src="<?php echo CMS_URL?>/assets/plugins/custom/prismjs/prismjs.bundle.js"></script>
     <script src="<?php echo CMS_URL?>/assets/js/scripts.bundle.js"></script>
     <!--end::Global Theme Bundle-->
     <!--begin::Page Scripts(used by this page)-->
-    <script src="<?php echo CMS_URL?>/assets/js/pages/custom/login/login-general.js"></script>
+    <script src="<?php echo CMS_URL?>/assets/js/pages/custom/login/login-3.js"></script>
     <!--end::Page Scripts-->
 </body>
 <!--end::Body-->
